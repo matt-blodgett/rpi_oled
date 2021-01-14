f8c02ff6f834fa733d0c5c10cdf45d0e9242033d
Matt Blodgett <mattblodgett94@gmail.com>
2021-01-14 00:25:07 -0500

    add requirements.txt and update install.sh instructions

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@ python3 -m venv venv
 . ./venv/bin/activate
 
 sudo pip3 install adafruit-circuitpython-ssd1306
-sudo apt-get install python3-pil
+# sudo apt install python3-pil
+sudo pip3 install Pillow
 
 # Check display is active
 sudo i2cdetect -y 1
+
+#     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+#00:          -- -- -- -- -- -- -- -- -- -- -- -- --
+#10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#30: -- -- -- -- -- -- -- -- -- -- -- -- 3c -- -- --
+#40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+#70: -- -- -- -- -- -- -- --
